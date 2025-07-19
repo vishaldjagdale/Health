2eb636c0ccb803269287db355e8443ccdcb3f500
adityagaikwad888 <adityakg99@gmail.com>
2025-07-19 21:37:17 +0530

    added updated jenkinsfile & server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:8000"],
+    origin: ["http://localhost:8000",],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
